13d50d73c0d313321554a1702d48ca67ff3a5068
Alexei <dark_sao@mail.ru>
2025-08-22 09:28:31 +0300

    sign up ability

--- cmd/web/handlers.go
+++ cmd/web/handlers.go
@@ -93,9 +93,23 @@ func (app *application) signupUser(w http.ResponseWriter, r *http.Request) {
 
 	if !form.Valid() {
 		app.render(w, r, "signup.page.tmpl", &templateData{Form: form})
+		return
 	}
 
-	fmt.Printf("Create new user")
+	err = app.users.Insert(form.Get("name"), form.Get("email"), form.Get("password"))
+	if err != nil {
+		if errors.Is(err, models.ErrDuplicateEmail) {
+			form.Errors.Add("email", err.Error())
+			app.render(w, r, "signup.page.tmpl", &templateData{Form: form})
+			return
+		} else {
+			app.serverError(w, err)
+			return
+		}
+	}
+
+	app.session.Put(r, "flash", "Your signup was successful. Please log in.")
+	http.Redirect(w, r, "/user/login", http.StatusSeeOther)
 }
 
 func (app *application) loginUserForm(w http.ResponseWriter, r *http.Request) {

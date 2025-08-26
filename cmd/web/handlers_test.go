535c097bf3dcbe5b80942bd8e7e08eb8f0627675
Alexei <dark_sao@mail.ru>
2025-08-26 09:43:26 +0300

    add test fot show snippet handler

--- cmd/web/handlers_test.go
+++ cmd/web/handlers_test.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"bytes"
 	"net/http"
 	"testing"
 )
@@ -19,5 +20,37 @@ func TestPing(t *testing.T) {
 
 	if string(body) != "OK" {
 		t.Errorf("want to body equal %q", "OK")
+	}
+}
+
+func TestShopwSnippet(t *testing.T) {
+	app := newTestApplication(t)
+	ts := newTestServer(app.routes())
+
+	defer ts.Close()
+
+	tests := []struct {
+		name     string
+		urlPath  string
+		wantCode int
+		wantBody []byte
+	}{
+		{"Valid ID", "/snippet/1", http.StatusOK, []byte("Test content")},
+		{"Non-existing ID", "/snippet/2", http.StatusNotFound, nil},
+		{"Not valid ID", "/snippet/-10", http.StatusNotFound, nil},
+	}
+
+	for _, test := range tests {
+		t.Run(test.name, func(t *testing.T) {
+			code, _, body := ts.Get(t, test.urlPath)
+
+			if code != test.wantCode {
+				t.Errorf("want %d; got %d", test.wantCode, code)
+			}
+
+			if !bytes.Contains(body, test.wantBody) {
+				t.Errorf("want body to contain %q", test.wantBody)
+			}
+		})
 	}
 }

1483951eaaf1aa3572ccd1e3dbc718f3f7374ea6
Alexei <dark_sao@mail.ru>
2025-08-27 09:55:28 +0300

    broken test

--- cmd/web/handlers_test.go
+++ cmd/web/handlers_test.go
@@ -3,12 +3,13 @@ package main
 import (
 	"bytes"
 	"net/http"
+	"net/url"
 	"testing"
 )
 
 func TestPing(t *testing.T) {
 	app := newTestApplication(t)
-	ts := newTestServer(app.routes())
+	ts := newTestServer(t, app.routes())
 
 	defer ts.Close()
 
@@ -25,7 +26,7 @@ func TestPing(t *testing.T) {
 
 func TestShopwSnippet(t *testing.T) {
 	app := newTestApplication(t)
-	ts := newTestServer(app.routes())
+	ts := newTestServer(t, app.routes())
 
 	defer ts.Close()
 
@@ -44,6 +45,142 @@ func TestShopwSnippet(t *testing.T) {
 		t.Run(test.name, func(t *testing.T) {
 			code, _, body := ts.Get(t, test.urlPath)
 
+			if code != test.wantCode {
+				t.Errorf("want %d; got %d", test.wantCode, code)
+			}
+
+			if !bytes.Contains(body, test.wantBody) {
+				t.Errorf("want body to contain %q", test.wantBody)
+			}
+		})
+	}
+}
+
+func TestSignUpUser(t *testing.T) {
+	app := newTestApplication(t)
+	ts := newTestServer(t, app.routes())
+
+	defer ts.Close()
+	_, _, body := ts.Get(t, "/user/signup")
+	csrfToken := extractCSRFToken(t, body)
+
+	t.Logf("CSRF token from form: %s", csrfToken)
+
+	// Посмотрим, что есть в cookie jar
+	for _, c := range ts.Client().Jar.Cookies(&url.URL{Scheme: "https", Host: ts.URL[8:]}) {
+		t.Logf("Cookie: %s = %s", c.Name, c.Value)
+	}
+
+	tests := []struct {
+		name      string
+		nameField string
+		email     string
+		password  string
+		csrfToken string
+		wantCode  int
+		wantBody  []byte
+	}{
+		{
+			name:      "Valid submission",
+			nameField: "Bob",
+			email:     "bob@example.com",
+			password:  "validPa$$word",
+			csrfToken: csrfToken,
+			wantCode:  http.StatusSeeOther,
+			wantBody:  nil,
+		},
+		{
+			name:      "Empty name",
+			nameField: "",
+			email:     "bob@example.com",
+			password:  "validPa$$word",
+			csrfToken: csrfToken,
+			wantCode:  http.StatusOK,
+			wantBody:  []byte("This field cannot be blank"),
+		},
+		{
+			name:      "Empty email",
+			nameField: "Bob",
+			email:     "",
+			password:  "validPa$$word",
+			csrfToken: csrfToken,
+			wantCode:  http.StatusOK,
+			wantBody:  []byte("This field cannot be blank"),
+		},
+		{
+			name:      "Empty password",
+			nameField: "Bob",
+			email:     "bob@example.com",
+			password:  "",
+			csrfToken: csrfToken,
+			wantCode:  http.StatusOK,
+			wantBody:  []byte("This field cannot be blank"),
+		},
+		{
+			name:      "Invalid email (incomplete domain)",
+			nameField: "Bob",
+			email:     "bob@example.",
+			password:  "validPa$$word",
+			csrfToken: csrfToken,
+			wantCode:  http.StatusOK,
+			wantBody:  []byte("This field is invalid"),
+		},
+		{
+			name:      "Invalid email (missing @)",
+			nameField: "Bob",
+			email:     "bobexample.com",
+			password:  "validPa$$word",
+			csrfToken: csrfToken,
+			wantCode:  http.StatusOK,
+			wantBody:  []byte("This field is invalid"),
+		},
+		{
+			name:      "Invalid email (missing local part)",
+			nameField: "Bob",
+			email:     "@example.com",
+			password:  "validPa$$word",
+			csrfToken: csrfToken,
+			wantCode:  http.StatusOK,
+			wantBody:  []byte("This field is invalid"),
+		},
+		{
+			name:      "Short password",
+			nameField: "Bob",
+			email:     "bob@example.com",
+			password:  "pa$$word",
+			csrfToken: csrfToken,
+			wantCode:  http.StatusOK,
+			wantBody:  []byte("This field is too short (minimum is 10 characters)"),
+		},
+		{
+			name:      "Duplicate email",
+			nameField: "Bob",
+			email:     "dupe@example.com",
+			password:  "validPa$$word",
+			csrfToken: csrfToken,
+			wantCode:  http.StatusOK,
+			wantBody:  []byte("Address is already in use"),
+		},
+		{
+			name:      "Invalid CSRF Token",
+			nameField: "Bob",
+			email:     "bob@example.com",
+			password:  "validPa$$word",
+			csrfToken: "wrongToken",
+			wantCode:  http.StatusBadRequest,
+			wantBody:  nil,
+		},
+	}
+
+	for _, test := range tests {
+		t.Run(test.name, func(t *testing.T) {
+			form := url.Values{}
+			form.Add("name", test.nameField)
+			form.Add("email", test.email)
+			form.Add("password", test.password)
+			form.Add("csrf_token", test.csrfToken)
+
+			code, _, body := ts.postForm(t, "/user/signup", form)
 			if code != test.wantCode {
 				t.Errorf("want %d; got %d", test.wantCode, code)
 			}
--- cmd/web/testutils_test.go
+++ cmd/web/testutils_test.go
@@ -1,17 +1,23 @@
 package main
 
 import (
+	"html"
 	"io"
+	"io/ioutil"
 	"log"
 	"net/http"
 	"net/http/cookiejar"
 	"net/http/httptest"
+	"net/url"
+	"regexp"
 	"testing"
 	"time"
 
 	"github.com/Onizukachi/snippetbox/pkg/models/mock"
 	"github.com/golangcollege/sessions"
 )
+
+var csrfTokenRx = regexp.MustCompile(`<input type=["']hidden["'] name=["']csrf_token["'] value=["']([^"']+)["']>`)
 
 func newTestApplication(t *testing.T) *application {
 	session := sessions.New([]byte("3dSm5MnygFHh7XidAtbskXrjbwfoJcbJ"))
@@ -36,10 +42,13 @@ type testServer struct {
 	*httptest.Server
 }
 
-func newTestServer(handler http.Handler) *testServer {
+func newTestServer(t *testing.T, handler http.Handler) *testServer {
 	ts := httptest.NewTLSServer(handler)
 
-	jar, _ := cookiejar.New(nil)
+	jar, err := cookiejar.New(nil)
+	if err != nil {
+		t.Fatal(err)
+	}
 	ts.Client().Jar = jar
 
 	ts.Client().CheckRedirect = func(req *http.Request, via []*http.Request) error {
@@ -63,4 +72,28 @@ func (ts *testServer) Get(t *testing.T, urlPath string) (int, http.Header, []byt
 	}
 
 	return rs.StatusCode, rs.Header, body
+}
+
+func (ts *testServer) postForm(t *testing.T, urlPath string, form url.Values) (int, http.Header, []byte) {
+	rs, err := ts.Client().PostForm(ts.URL+urlPath, form)
+	if err != nil {
+		t.Fatal(err)
+	}
+	// Read the response body.
+	defer rs.Body.Close()
+	body, err := ioutil.ReadAll(rs.Body)
+	if err != nil {
+		t.Fatal(err)
+	}
+	// Return the response status, headers and body.
+	return rs.StatusCode, rs.Header, body
+}
+
+func extractCSRFToken(t *testing.T, body []byte) string {
+	matches := csrfTokenRx.FindSubmatch(body)
+	if len(matches) < 2 {
+		t.Fatal("no csrf token found in body")
+	}
+
+	return html.UnescapeString(string(matches[1]))
 }
